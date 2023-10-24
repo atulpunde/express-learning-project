1902ec061e5ea14e41d3f0588e7cde572fc15b8c
Atul Punde <atulpunde970@gmail.com>
2023-10-24 16:43:19 +0530

    removed commented code and added doc comments

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-
+// Using handlebars in first express app
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
-// app.use(express.static(path.join(__dirname, 'static')))
-
+// Link the routes file
 app.use('/', require(path.join(__dirname, 'routes/blog.js')))
 
+// Create server which will listen on some port
 app.listen(port, () => {
     console.log("Blog app is listning at http://localhost:"+ port)
 })
